1cd6315d33c8b5232bddf0b94eda1445b71ea244
pro-6 <pro-6@pro-6deMacBook-Pro.local>
2018-12-20 10:10:00 +0800

    react-native video 引入

--- src/screens/main/index.js
+++ src/screens/main/index.js
@@ -1,5 +1,7 @@
 import React, { PureComponent } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
+import Video from 'react-native-video';
+
 export default class MainScreen extends PureComponent {
     static navigationOptions = ({ navigation }) => {
         return {
@@ -7,10 +9,23 @@ export default class MainScreen extends PureComponent {
         }
     };
 
+    onBuffer = () => {
+        console.log('is on buffer');
+    }
+    videoError = () => {
+        console.log('is on error');
+    }
+
     render() {
         return (
             <View style={styles.container}>
-                <Text>MainScreen</Text>
+                <Video
+                    source={{ uri: 'https://t.bwzybf.com/2018/11/12/d0Kixl229jxIo36g/playlist.m3u8' }}
+                    ref={(ref) => { this.player = ref }}
+                    onBuffer={this.onBuffer}
+                    onError={this.videoError}
+                    style={styles.bgVideo}
+                />
             </View>
         );
     }
@@ -21,5 +36,12 @@ const styles = StyleSheet.create({
         justifyContent: 'center',
         alignItems: 'center',
         backgroundColor: 'white',
+    },
+    bgVideo: {
+        position: 'absolute',
+        top: 0,
+        left: 0,
+        bottom: 0,
+        right: 0
     }
 });
