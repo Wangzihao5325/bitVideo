eed6cdb7276cd985ff1656aad51f10ef7ab7f15d
pro-6 <pro-6@pro-6deMacBook-Pro.local>
2019-01-03 12:18:39 +0800

    1.引入并修改tabbar

---
+++ src/components/tabBar/index.js
@@ -0,0 +1,121 @@
+import React, { PureComponent, Component } from 'react';
+import { View, Dimensions, StyleSheet, ScrollView, Text, TouchableHighlight } from 'react-native';
+import PropsTypes from 'prop-types';
+
+const DEVICE_WIDTH = Dimensions.get('window').width;
+const THEME_COLOR = 'transparent';
+const CONTAINER_BORDER_COLOR = '#909090';
+const NORMAL_TEXT_COLOR = 'white';//'#909090'
+const ITEM_HIGHLIGHT_COLOR = 'white';//'rgb(0,122,204)'
+const ITEM_MARGIN_HOR = 5;
+const ITEM_MARGIN_VER = 2;
+
+class Item extends PureComponent {
+    static propTypes = {
+        keyValue: PropsTypes.number,
+        highlightIndex: PropsTypes.number,
+        itemTitle: PropsTypes.string,
+        itemTap: PropsTypes.func,
+    }
+    state = {
+        isHighLight: false
+    }
+    static getDerivedStateFromProps(nextProps, prevState) {
+        if (nextProps.highlightIndex === nextProps.keyValue) {
+            return { isHighLight: true }
+        } else {
+            return { isHighLight: false }
+        }
+    }
+    _itemPress = () => {
+        if (this.state.isHighLight) { return }
+        this.props.itemTap(this.props.keyValue);
+    }
+    render() {
+        let itemStyle = this.state.isHighLight ? null : null;//styles.itemContainerHighlight//选中按钮底部蓝色下划线
+        let textStyle = this.state.isHighLight ? styles.itemTextHighlight : null;
+        return (
+            <TouchableHighlight style={[styles.itemContainer, itemStyle]} onPress={this._itemPress} underlayColor={'transparent'}>
+                <Text style={[styles.itemText, textStyle]}>{this.props.itemTitle}</Text>
+            </TouchableHighlight>
+        );
+    }
+}
+export default class TabBar extends PureComponent {
+    static propTypes = {
+        tabNames: PropsTypes.array,
+        tabTap: PropsTypes.func
+    }
+
+    state = {
+        highlightIndex: 0
+    }
+
+    _itemTap = (keyValue) => {
+        let oldstate = this.props.tabNames[this.state.highlightIndex];
+        this.setState({ highlightIndex: keyValue });
+        this.props.tabTap(this.props.tabNames[keyValue], oldstate);
+    }
+
+    itemsGenerator = (tabNames) => {
+        let items = [];
+        tabNames.forEach((item, index) => {
+            items.push(
+                <Item
+                    key={index}
+                    keyValue={index}
+                    highlightIndex={this.state.highlightIndex}
+                    itemTitle={item}
+                    itemTap={this._itemTap}
+                />
+            );
+        });
+        return items;
+    }
+
+    render() {
+        let items = this.itemsGenerator(this.props.tabNames);
+        return (
+            <View style={styles.container}>
+                <ScrollView
+                    horizontal={true}
+                    alwaysBounceHorizontal={false}
+                    showsHorizontalScrollIndicator={false}
+                    showsVerticalScrollIndicator={false}
+                >
+                    {items}
+                </ScrollView>
+            </View>
+        );
+    }
+}
+const styles = StyleSheet.create({
+    container: {
+        display: 'flex',
+        width: DEVICE_WIDTH,
+        // borderBottomColor: CONTAINER_BORDER_COLOR,//底部浅灰色分割线
+        // borderBottomWidth: StyleSheet.hairlineWidth,
+        backgroundColor: THEME_COLOR
+    },
+    itemContainer: {
+        display: 'flex',
+        height: 30,
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginVertical: ITEM_MARGIN_VER,
+        marginHorizontal: ITEM_MARGIN_HOR
+    },
+    itemText: {
+        color: NORMAL_TEXT_COLOR
+    },
+    itemContainerHighlight: {
+        marginBottom: ITEM_MARGIN_VER - 2,
+        borderBottomColor: ITEM_HIGHLIGHT_COLOR,
+        borderBottomWidth: 2,
+        borderStyle: 'solid',
+    },
+    itemTextHighlight: {
+        color: ITEM_HIGHLIGHT_COLOR,
+        fontSize: 18
+    },
+});
--- src/screens/main/index.js
+++ src/screens/main/index.js
@@ -1,8 +1,9 @@
 import React, { PureComponent } from 'react';
-import { SafeAreaView, StyleSheet } from 'react-native';
-import WebView from 'react-native-android-fullscreen-webview-video';
+import { SafeAreaView, StyleSheet, View } from 'react-native';
 import * as PixelUtil from '../../global/utils/PixelUtil';
 import * as Colors from '../../global/Colors';
+
+import TabBar from '../../components/tabBar/index';
 
 export default class MainScreen extends PureComponent {
     static navigationOptions = ({ navigation }) => {
@@ -11,48 +12,33 @@ export default class MainScreen extends PureComponent {
         }
     };
 
-    onBuffer = () => {
-        console.log('is on buffer');
-    }
-    videoError = () => {
-        console.log('is on error');
-    }
-
     render() {
 
         let WV_Width = PixelUtil.webviewSizeUnifyFromDPToPX(200);
         let WV_Height = PixelUtil.webviewSizeUnifyFromDPToPX(150);
         return (
-            <SafeAreaView style={styles.container}>
-                <WebView
-                    originWhitelist={['*']}
-                    source={{
-                        html:
-                            `<div>
-                                <iframe 
-                                  width="${WV_Width}" 
-                                  height="${WV_Height}" 
-                                  src="https://me.guiji365.com/share/AzUc08XrXJAC6Or7" 
-                                  frameborder="0" 
-                                >
-                                </iframe> 
-                            </div> `
-                    }}
-                />
+            <SafeAreaView style={styles.safeArea}>
+                <View style={styles.container}>
+                    <View style={styles.headerContainer}>
+                        <TabBar tabNames={['推荐', '搞笑', '电视剧', '动漫', '综艺']} tabTap={(classfiy) => { console.log(classfiy) }} />
+                    </View>
+                </View>
             </SafeAreaView>
         );
     }
 }
 const styles = StyleSheet.create({
-    container: {
+    safeArea: {
         flex: 1,
         backgroundColor: Colors.SAFE_AREA_BGCOLOR
     },
-    bgVideo: {
-        position: 'absolute',
-        top: 0,
-        left: 0,
-        bottom: 0,
-        right: 0
+    container: {
+        flex: 1,
+        backgroundColor: 'white'
+    },
+    headerContainer: {
+        display: 'flex',
+        height: 75,
+        backgroundColor: Colors.SAFE_AREA_BGCOLOR
     }
 });
