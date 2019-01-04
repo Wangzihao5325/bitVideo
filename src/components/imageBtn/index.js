757b64e834b22d49a76dfa17a6a5815cc29d81ea
pro-6 <pro-6@pro-6deMacBook-Pro.local>
2019-01-04 17:50:00 +0800

    merge

--- src/components/imageBtn/index.js
+++ src/components/imageBtn/index.js
@@ -1,9 +1,18 @@
 import React, { PureComponent } from 'react';
-import { View, TouchableHighlight, ImageBackground } from 'react-native';
-// class TypeOneBtn extends PureComponent{
-//     render(){
-//         return(
-
-//         );
-//     }
-// }
+import { StyleSheet, View, TouchableHighlight, ImageBackground } from 'react-native';
+class TypeOneBtn extends PureComponent {
+    render() {
+        return (
+            <TouchableHighlight style={styles.container}>
+                <View></View>
+            </TouchableHighlight>
+        );
+    }
+}
+const styles = StyleSheet.create({
+    container: {
+        display: 'flex',
+        height: 90,
+        width: 60
+    }
+});
