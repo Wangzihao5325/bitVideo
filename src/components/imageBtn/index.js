2c87da5f6cfa6b12f4189d0e4bd1d83e2015c905
pro-6 <pro-6@pro-6deMacBook-Pro.local>
2019-01-04 10:23:23 +0800

    1.增加image btn

---
+++ src/components/imageBtn/index.js
@@ -0,0 +1,9 @@
+import React, { PureComponent } from 'react';
+import { View, TouchableHighlight, ImageBackground } from 'react-native';
+// class TypeOneBtn extends PureComponent{
+//     render(){
+//         return(
+
+//         );
+//     }
+// }
