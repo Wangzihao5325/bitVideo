8ef30b9b20913fb098fc83b58dda763bdd535e04
pro-6 <pro-6@pro-6deMacBook-Pro.local>
2019-01-04 18:06:11 +0800

    1.增加mine background

--- src/screens/mine/index.js
+++ src/screens/mine/index.js
@@ -1,19 +1,31 @@
 import React, { PureComponent } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, ImageBackground, SafeAreaView } from 'react-native';
 export default class MineScreen extends PureComponent {
+    static navigationOptions = ({ navigation }) => {
+        return {
+            header: null
+        }
+    };
+
     render() {
         return (
-            <View style={styles.container}>
-                <Text>MineScreen</Text>
-            </View>
+            <ImageBackground
+                style={styles.imageBackground}
+                source={require('../../image/mine/mine_background.png')}
+            >
+                <SafeAreaView style={styles.container}>
+
+                </SafeAreaView>
+            </ImageBackground>
         );
     }
 }
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: 'white',
+    },
+    imageBackground: {
+        width: '100%',
+        height: 207
     }
 });
