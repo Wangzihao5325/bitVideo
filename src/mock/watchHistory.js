7e4441107c3d87152262b39c153695dbb58304e2
pro-6 <pro-6@pro-6deMacBook-Pro.local>
2019-01-05 15:55:35 +0800

    1.增加mock数据，优化观看历史

---
+++ src/mock/watchHistory.js
@@ -0,0 +1,123 @@
+export default watchHistory = [
+    {
+        "id": 3,
+        "title": "奇幻梦之旅",
+        "cover_path": "http://pic1.iqiyipic.com/image/20181122/dd/ce/v_120367467_m_601_m3_180_236.jpg",
+        "global_type": "film",
+        "dist_id": 2,
+        "epoch_id": "2018",
+        "director": null,
+        "play_count": 101,
+        "created_at": null,
+        "label_status": 0,
+        "intro": "成功人士王大河遭人陷害，失去一切。在低迷",
+        "status_label": "",
+        "dist_label": "大陆电影",
+        "type": [
+            {
+                "id": 3,
+                "video_id": 3,
+                "global_type": "film",
+                "type_id": 2,
+                "created_at": null,
+                "updated_at": null,
+                "deleted_at": null,
+                "type_label": "喜剧"
+            },
+            {
+                "id": 24,
+                "video_id": 3,
+                "global_type": "film",
+                "type_id": 7,
+                "created_at": null,
+                "updated_at": null,
+                "deleted_at": null,
+                "type_label": "爱情"
+            },
+            {
+                "id": 25,
+                "video_id": 3,
+                "global_type": "film",
+                "type_id": 12,
+                "created_at": null,
+                "updated_at": null,
+                "deleted_at": null,
+                "type_label": "奇幻"
+            }
+        ]
+    },
+    {
+        "id": 1,
+        "title": "海王",
+        "cover_path": "https://pic1.iqiyipic.com/image/20181006/92/15/v_119649714_m_601_260_360.jpg",
+        "global_type": "film",
+        "dist_id": 1,
+        "epoch_id": "2018",
+        "director": null,
+        "play_count": 100,
+        "created_at": "2018-12-12 06:39:36",
+        "label_status": 0,
+        "intro": "华纳兄弟影片公司与导演温子仁联手为您呈现",
+        "status_label": "",
+        "dist_label": "欧美电影",
+        "type": [
+            {
+                "id": 1,
+                "video_id": 1,
+                "global_type": "film",
+                "type_id": 1,
+                "created_at": null,
+                "updated_at": "2018-12-08 02:40:50",
+                "deleted_at": null,
+                "type_label": "动作"
+            },
+            {
+                "id": 22,
+                "video_id": 1,
+                "global_type": "film",
+                "type_id": 3,
+                "created_at": null,
+                "updated_at": "2018-12-08 02:40:50",
+                "deleted_at": null,
+                "type_label": "科幻"
+            }
+        ]
+    },
+    {
+        "id": 2,
+        "title": "英雄归来",
+        "cover_path": "http://pic7.iqiyipic.com/image/20181207/b6/f3/v_112865025_m_601_m4_180_236.jpg",
+        "global_type": "film",
+        "dist_id": 1,
+        "epoch_id": "2018",
+        "director": null,
+        "play_count": 100,
+        "created_at": null,
+        "label_status": 0,
+        "intro": "装腔作势的纽维尔将军在结婚前夕收到徵召令",
+        "status_label": "",
+        "dist_label": "欧美电影",
+        "type": [
+            {
+                "id": 2,
+                "video_id": 2,
+                "global_type": "film",
+                "type_id": 1,
+                "created_at": null,
+                "updated_at": null,
+                "deleted_at": null,
+                "type_label": "动作"
+            },
+            {
+                "id": 23,
+                "video_id": 2,
+                "global_type": "film",
+                "type_id": 14,
+                "created_at": null,
+                "updated_at": null,
+                "deleted_at": null,
+                "type_label": "历史"
+            }
+        ]
+    }
+];
--- src/screens/mine/history.js
+++ src/screens/mine/history.js
@@ -1,6 +1,8 @@
 import React, { PureComponent } from 'react';
-import { View, StyleSheet, Image, Text, TouchableHighlight } from 'react-native';
+import { View, StyleSheet, Image, Text, TouchableHighlight, FlatList } from 'react-native';
 import * as In18 from '../../global/In18';
+
+import watchHistory from '../../mock/watchHistory';
 
 class Header extends PureComponent {
     moreHistory = () => {
@@ -23,11 +25,43 @@ class Header extends PureComponent {
         );
     }
 }
+class Item extends PureComponent {
+    watchHistoryPressing = () => {
+        console.log('go to see history movie');
+    }
+    render() {
+        return (
+            <TouchableHighlight style={styles.itemContainer} onPress={this.watchHistoryPressing} underlayColor='transparent'>
+                <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+                    <Image style={styles.itemImage} source={this.props.source} />
+                    <View style={styles.itemTextContainer}>
+                        <Text style={styles.itemTitle}>{this.props.title}</Text>
+                    </View>
+                </View>
+            </TouchableHighlight>
+        );
+    }
+}
+class Snap extends PureComponent {
+    render() {
+        return (
+            <FlatList
+                style={styles.flatList}
+                showsVerticalScrollIndicator={false}
+                showsHorizontalScrollIndicator={false}
+                horizontal={true}
+                data={watchHistory}
+                renderItem={({ item }) => <Item source={{ uri: item.cover_path }} title={item.title} />}
+            />
+        );
+    }
+}
 export default class History extends PureComponent {
     render() {
         return (
             <View>
                 <Header />
+                <Snap />
             </View>
         );
     }
@@ -67,5 +101,31 @@ const styles = StyleSheet.create({
         height: 16,
         width: 16,
         marginLeft: 6
+    },
+    itemContainer: {
+        height: 175 + 20 + 7,
+        width: 129,
+    },
+    itemImage: {
+        height: 175,
+        width: 125,
+        borderRadius: 5
+    },
+    itemTextContainer: {
+        height: 20,
+        marginTop: 7,
+        width: 125,
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    itemTitle: {
+        color: 'rgb(33,33,33)',
+        fontSize: 14
+    },
+    flatList: {
+        height: 179 + 20 + 7,
+        width: '100%',
+        marginTop: 20
     }
 });
