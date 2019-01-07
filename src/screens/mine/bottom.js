32dd38349cde0a03f9114ad206b30d646e47fe7c
pro-6 <pro-6@pro-6deMacBook-Pro.local>
2019-01-07 12:49:20 +0800

    1.优化部分icon

--- src/screens/mine/bottom.js
+++ src/screens/mine/bottom.js
@@ -28,8 +28,8 @@ export default class Bottom extends PureComponent {
     render() {
         return (
             <View style={{ paddingBottom: 20 }} >
-                <Item source={require('../../image/mine/myCache.png')} title='我的收藏' onPress={this.myStar} />
-                <Item source={require('../../image/mine/myCache.png')} title='帮助反馈' onPress={this.getHelp} />
+                <Item source={require('../../image/mine/my_video.png')} title='我的收藏' onPress={this.myStar} />
+                <Item source={require('../../image/mine/get_help.png')} title='帮助反馈' onPress={this.getHelp} />
             </View>
         );
     }
