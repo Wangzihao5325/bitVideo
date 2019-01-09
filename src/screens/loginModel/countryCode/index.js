b48e8314f004a3a3b2be8f8c55b55e1927f6e726
pro-6 <pro-6@pro-6deMacBook-Pro.local>
2019-01-09 17:58:11 +0800

    1.normal merge

--- src/screens/loginModel/countryCode/index.js
+++ src/screens/loginModel/countryCode/index.js
@@ -1,9 +1,18 @@
 import React, { PureComponent } from 'react';
-import { SafeAreaView } from 'react-native';
+import { SafeAreaView, SectionList, View } from 'react-native';
 import * as In18 from '../../../global/In18';
 
 import ModalHeader from '../modalComponent/ModalHeader';
 
+class Item extends PureComponent {
+    render() {
+        return (
+            <View>
+
+            </View>
+        );
+    }
+}
 export default class CountryCode extends PureComponent {
     goBack = () => {
         this.props.navigation.goBack()
@@ -12,6 +21,9 @@ export default class CountryCode extends PureComponent {
         return (
             <SafeAreaView>
                 <ModalHeader title={In18.COUNTRY_CODE} goBack={this.goBack} />
+                <SectionList
+                    renderItem={({ item, index, section }) => { }}
+                />
             </SafeAreaView>
         );
     }
