280be049aac411f3e320f2c8659c462e110e32c5
pro-6 <pro-6@pro-6deMacBook-Pro.local>
2019-01-14 18:33:50 +0800

    1.手机号加*

--- src/screens/loginModel/register/RegisterStepTwo.js
+++ src/screens/loginModel/register/RegisterStepTwo.js
@@ -1,21 +1,27 @@
 import React, { PureComponent } from 'react';
 import { SafeAreaView, Text, TouchableHighlight, StyleSheet } from 'react-native';
+import { connect } from 'react-redux';
+import store from '../../../store/index';
 import * as In18 from '../../../global/In18';
+import { registerReg } from '../../../global/Reg';
 
 import ModalHeader from '../modalComponent/ModalHeader';
 import PasswordInputWithVerificationCode from '../../../components/input/PasswordInputWithVerificationCode';
 import PasswordInput from '../../../components/input/PasswordInput';
 
-export default class RegisterStepTwoScreen extends PureComponent {
+class RegisterStepTwoScreen extends PureComponent {
     goBack = () => {
         this.props.navigation.goBack()
     }
     render() {
+        let mobile = registerReg.mobile.split('');
+        mobile.splice(3, 4, '****');
+        let securityMobile = mobile.join('');
         return (
             <SafeAreaView >
                 <ModalHeader title={In18.INPUT_VER_CODE} goBack={this.goBack} />
                 <Text style={styles.titleText}>{In18.CLICK_TO_GET_VER_CODE}</Text>
-                <Text style={styles.mobileText}>+86187****1234</Text>
+                <Text style={styles.mobileText}>{`${this.props.countryCode} `}<Text>{securityMobile}</Text></Text>
                 <PasswordInputWithVerificationCode style={{ marginTop: 48 }} />
                 <PasswordInput placeHolder={In18.PLEASE_SET_PASSWORD} style={{ marginTop: 53 }} />
                 <TouchableHighlight style={styles.btn}>
@@ -25,6 +31,14 @@ export default class RegisterStepTwoScreen extends PureComponent {
         );
     }
 }
+function mapState2Props(store) {
+    return {
+        countryCode: store.countryCode.countryCode
+    }
+}
+
+export default connect(mapState2Props)(RegisterStepTwoScreen);
+
 const styles = StyleSheet.create({
     titleText: {
         marginTop: 80,
