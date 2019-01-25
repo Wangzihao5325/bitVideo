cf57c735fe28760a91d57f2d7fc5353365059124
pro-6 <pro-6@pro-6deMBP.mshome.net>
2019-01-25 21:04:05 +0800

    1.video module btn增加

--- src/components/modules/video.js
+++ src/components/modules/video.js
@@ -1,9 +1,18 @@
 import React, { PureComponent } from 'react';
-import { FlatList, View } from 'react-native';
+import { StyleSheet, FlatList, View, Text, TouchableHighlight } from 'react-native';
 
 import VideoAvater from '../imageBtn/VideoAvater';
 import TitleHeader from '../../components/titleHeader/index';
 
+class TextBtn extends PureComponent {
+    render() {
+        return (
+            <TouchableHighlight style={styles.btn} underlayColor='transparent' onPress={this.props.btnPress}>
+                <Text style={styles.btnText}>{this.props.title}</Text>
+            </TouchableHighlight>
+        );
+    }
+}
 class SUDOKU extends PureComponent {
     state = {
         data: []
@@ -17,7 +26,7 @@ class SUDOKU extends PureComponent {
     }
     render() {
         return (
-            <View style={{ flex: 1, alignItems: 'center' }}>
+            <View style={{ flex: 1, alignItems: 'center', paddingBottom: 3 }}>
                 <TitleHeader style={{ marginTop: 20 }} imageSource={require('../../image/main/global_movie.png')} title={this.props.title} />
                 <FlatList
                     horizontal={false}
@@ -26,6 +35,10 @@ class SUDOKU extends PureComponent {
                     renderItem={({ item }) => <VideoAvater isVertical={this.props.isVertical} onPress={() => this.movieAvaterOnPress(item.id)} imageSource={{ uri: `${item.cover_path}` }} title={item.title} info={item.intro} />}
                     columnWrapperStyle={{ marginTop: 20 }}
                 />
+                <View style={{ flex: 1, flexDirection: 'row', marginTop: 20 }}>
+                    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}><TextBtn title='更多' btnPress={() => console.log('111')} /></View>
+                    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}><TextBtn title='换一换' btnPress={() => console.log('222')} /></View>
+                </View>
             </View>
         );
     }
@@ -49,3 +62,18 @@ export default class VideoModule extends PureComponent {
         }
     }
 }
+
+const styles = StyleSheet.create({
+    btn: {
+        height: 40,
+        width: 170,
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: 'rgb(239,244,247)'
+    },
+    btnText: {
+        color: 'rgb(187,186,186)',
+        fontSize: 14
+    }
+});
