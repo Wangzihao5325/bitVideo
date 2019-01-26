ad777055eef1853e8856ffd8196d5211ec3b0502
pro-6 <pro-6@pro-6deMBP.mshome.net>
2019-01-26 12:15:05 +0800

    1.normal merge

--- src/screens/main/content.js
+++ src/screens/main/content.js
@@ -25,38 +25,12 @@ class Item extends PureComponent {
         }
     }
 }
-// class Content extends PureComponent {
-//     render() {
-//         let modules = [];
-//         if (this.props.data && this.props.data.length > 0) {
-//             this.props.data.forEach((item, index) => {
-//                 switch (item.client_module) {
-//                     case 'm_banner':
-//                         if (item.m_banner_data.length > 0) {//有可能会出现 arr.length=0 的情况
-//                             modules.push(<BannerModule key={index} data={item.m_banner_data} />);
-//                         }
-//                         break;
-//                     case 'm_video':
-//                         modules.push(<VideoModule title={item.title} key={index} limit={item.client_limit} clientStyle={item.client_style} data={item.m_video_data} />);
-//                         break;
-//                     case 'm_ad':
-//                         modules.push(<AdModule key={index} data={item.m_ad_data[0]} />);
-//                         break;
-//                 }
-//             })
-//         }
-//         return (
-//             <ScrollView showsVerticalScrollIndicator={false} contentContainerStyle={{ display: 'flex', alignItems: 'center' }}>
-//                 {modules}
-//             </ScrollView>
-//         );
-//     }
-// }
 
 class Content extends PureComponent {
     render() {
         return (
             <FlatList
+                showsVerticalScrollIndicator={false}
                 data={this.props.data}
                 renderItem={({ item, index }) => <Item item={item} index={index} />}
             />
