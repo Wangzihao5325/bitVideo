be14a1d4325f7e180e8112dd1f8eaafd4f162001
pro-6 <pro-6@pro-6deMBP.mshome.net>
2019-01-29 18:30:34 +0800

    1.忽略bundle文件

--- ios/BitVideos/AppDelegate.m
+++ ios/BitVideos/AppDelegate.m
@@ -16,11 +16,11 @@
 {
   NSURL *jsCodeLocation;
 
-  #ifdef DEBUG
+//  #ifdef DEBUG
     jsCodeLocation = [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index" fallbackResource:nil];
-  #else
-    jsCodeLocation = [[NSBundle mainBundle] URLForResource:@"bundle/index" withExtension:@"jsbundle"];
-  #endif
+//  #else
+//    jsCodeLocation = [[NSBundle mainBundle] URLForResource:@"main" withExtension:@"jsbundle"];
+//  #endif
 
   RCTRootView *rootView = [[RCTRootView alloc] initWithBundleURL:jsCodeLocation
                                                       moduleName:@"BitVideos"
