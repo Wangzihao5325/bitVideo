13e74d172c47facdd11bb287865bc931ec908c28
pro-6 <pro-6@pro-6deMacBook-Pro.local>
2019-02-18 11:39:10 +0800

    1.normal merge

--- src/screens/historyModel/index.js
+++ src/screens/historyModel/index.js
@@ -1,9 +1,10 @@
 import React, { PureComponent } from 'react';
-import { SafeAreaView, View } from 'react-native';
+import { SafeAreaView, View, FlatList } from 'react-native';
+import { connect } from 'react-redux';
 
 import ModalHeader from '../../components/modal/ModalHeader';
 
-export default class HistoryModel extends PureComponent {
+class HistoryModel extends PureComponent {
     _editMode = () => {
         console.log('change edit mode');
     }
@@ -14,7 +15,16 @@ export default class HistoryModel extends PureComponent {
         return (
             <SafeAreaView style={{ flex: 1 }}>
                 <ModalHeader goBack={this._goBack} title='我的播放记录' rightBtnMode='text' rightBtnTitle='编辑' rightBtnOnPress={this._editMode} />
+                <FlatList />
             </SafeAreaView>
         );
     }
 }
+
+function mapState2Props(store) {
+    return {
+        data: store.watchHistory.historyMovies,
+    }
+}
+
+export default connect(mapState2Props)(HistoryModel);
