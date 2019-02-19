c1097bdb1f6315cf85f2f28941dc667687bcc60a
pro-6 <pro-6@pro-6deMacBook-Pro.local>
2019-02-19 12:03:03 +0800

    1.view moudle more page优化

--- src/screens/main/childrenPage/ViewModuleMoreScreen.js
+++ src/screens/main/childrenPage/ViewModuleMoreScreen.js
@@ -1,8 +1,8 @@
 import React, { PureComponent } from 'react';
-import { SafeAreaView, View } from 'react-native';
+import { SafeAreaView, View, FlatList } from 'react-native';
 import Api from '../../../socket/index';
 
-import ModalHeader from '../../../components/modal/ModalHeader';
+import VideoAvater from '../../../components/imageBtn/VideoAvater';
 
 export default class ViewModuleMoreScreen extends PureComponent {
     static navigationOptions = ({ navigation }) => {
@@ -11,12 +11,17 @@ export default class ViewModuleMoreScreen extends PureComponent {
         };
     };
 
+    state = {
+        data: []
+    };
+
     componentDidMount() {
         const moduleId = this.props.navigation.getParam('moduleId', 'undefine_Id');
-        Api.getViewModuleMore(moduleId, 1, 10, (e) => {
-            if (e) {
-                console.log('ffffff');
-                console.log(e);
+        Api.getViewModuleMore(moduleId, 1, 15, (e) => {
+            if (e && e.data) {
+                this.setState({
+                    data: e.data
+                });
             }
         })
     }
@@ -25,9 +30,22 @@ export default class ViewModuleMoreScreen extends PureComponent {
         this.props.navigation.pop();
     }
 
+    _videoAvaterOnPress = (id) => {
+        this.props.navigation.navigate('VideoModel', { videoId: id });
+    }
+
     render() {
         return (
             <SafeAreaView style={{ flex: 1 }}>
+                {this.state.data.length > 0 &&
+                    <FlatList
+                        data={this.state.data}
+                        contentContainerStyle={{ alignSelf: 'center' }}
+                        columnWrapperStyle={{ marginTop: 20 }}
+                        showsVerticalScrollIndicator={false}
+                        numColumns={3}
+                        renderItem={({ item }) => <VideoAvater isVertical={true} onPress={() => this._videoAvaterOnPress(item.id)} imageSource={{ uri: `${item.cover_path}` }} title={item.title} info={item.intro} score={item.score} />}
+                    />}
             </SafeAreaView>
         );
     }
