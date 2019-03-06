d0735d6d528fac7e70324c2e00b823f9033894cf
Wangzihao5325 <416990273@qq.com>
2019-03-06 13:59:52 +0700

    1.security image内存泄漏处理

--- src/components/securtyImage/index.js
+++ src/components/securtyImage/index.js
@@ -6,60 +6,45 @@ import RNFetchBlob from 'rn-fetch-blob';
 
 export default class SecurtyImage extends PureComponent {
     state = {
-        source: null,
-        lastUri: ''
-    }
-
-    static getDerivedStateFromProps(nextProps, preState) {
-        let uri = nextProps.source.uri;
-        if (uri && preState.lastUri !== uri) {
-            if (uri.indexOf('data:image/png;base64,') >= 0) {
-                return { source: nextProps.source, decode: false, lastUri: uri };;
-            }
-            let regArr = uri.split('.');
-            let typeStr = regArr[regArr.length - 1];
-            if (typeStr === 'ceb') {
-                return { source: null, decode: true, lastUri: uri }
-            }
-            return { source: nextProps.source, decode: false, lastUri: uri };;
-        } else if (uri && preState.lastUri === uri) {
-            let stateUri = preState.source.uri;
-            if (stateUri && stateUri.indexOf('data:image/png;base64,') >= 0) {
-                return null;
-            }
-            return { decode: false };
-        }
-        return { source: nextProps.source, decode: false, lastUri: nextProps.source };
+        trueSource: null,
+        lastSource: null
     }
 
     componentDidMount() {
-        this._getDataAndDecode();
+        this._getTrueSource();
     }
 
     componentDidUpdate() {
-        this._getDataAndDecode();
+        this._getTrueSource();
     }
 
-    _getDataAndDecode = () => {
-        if (this.state.decode) {
-            RNFetchBlob
-                .config({ fileCache: true })
-                .fetch('GET', this.props.source.uri)
-                .then((res) => {
-                    let filePath = res.path();
-                    let fs = RNFetchBlob.fs;
-                    fs.readFile(filePath, 'base64')//utf8//base64
-                        .then(data => {
-                            AESImageUtils.decryptFromJSBase64(data).then((e) => {
-                                if (e.result) {
-                                    // DeviceEventEmitter.emit('bitImage', e.result);
-                                    this.setState({
-                                        source: { uri: 'data:image/png;base64,' + e.result, decode: false }
-                                    });
-                                }
+    _getTrueSource = () => {
+        let uri = this.props.source.uri;
+        if (uri !== this.state.lastSource) {
+            let regArr = uri.split('.');
+            let typeStr = regArr[regArr.length - 1];
+            if (typeStr === 'ceb') {
+                RNFetchBlob
+                    .config({ fileCache: true })
+                    .fetch('GET', uri)
+                    .then((res) => {
+                        let filePath = res.path();
+                        let fs = RNFetchBlob.fs;
+                        fs.readFile(filePath, 'base64')//utf8//base64
+                            .then(data => {
+                                AESImageUtils.decryptFromJSBase64(data).then((e) => {
+                                    if (e.result) {
+                                        this.setState({
+                                            source: { uri: 'data:image/png;base64,' + e.result }
+                                        });
+                                    }
+                                });
                             });
-                        });
-                });
+                    });
+                this.setState({ lastSource: uri });
+            } else {
+                this.setState({ trueSource: this.props.source, lastSource: uri });
+            }
         }
     }
 
