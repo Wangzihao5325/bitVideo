c7aa6a15a7fa487dfb193468557d74328b2f5f37
Wangzihao5325 <416990273@qq.com>
2019-04-11 21:45:40 +0700

    1.引入启动页npm包

--- android/app/src/main/java/com/bitvideos/MainApplication.java
+++ android/app/src/main/java/com/bitvideos/MainApplication.java
@@ -16,6 +16,7 @@ import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
+import org.devio.rn.splashscreen.SplashScreenReactPackage;
 
 import java.util.Arrays;
 import java.util.List;
@@ -32,6 +33,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+              new SplashScreenReactPackage(),
             new RNViewShotPackage(),
             new FPStaticServerPackage(),
             new RNFetchBlobPackage(),
