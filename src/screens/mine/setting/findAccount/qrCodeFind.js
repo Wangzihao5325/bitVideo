54635fdf1777cada3361d01c340fc2734374da73
Wangzihao5325 <416990273@qq.com>
2019-04-30 20:49:45 +0700

    1.增加二维码扫码功能

--- src/screens/mine/setting/findAccount/qrCodeFind.js
+++ src/screens/mine/setting/findAccount/qrCodeFind.js
@@ -28,16 +28,16 @@ export default class QrCodeFind extends PureComponent {
                 />
                 <QRCodeScanner
                     onRead={this.onSuccess.bind(this)}
-                    topContent={
-                        <Text style={styles.centerText}>
-                            Go to <Text style={styles.textBold}>wikipedia.org/wiki/QR_code</Text> on your computer and scan the QR code.
-          </Text>
-                    }
-                    bottomContent={
-                        <TouchableOpacity style={styles.buttonTouchable}>
-                            <Text style={styles.buttonText}>OK. Got it!</Text>
-                        </TouchableOpacity>
-                    }
+                //             topContent={
+                //                 <Text style={styles.centerText}>
+                //                     Go to <Text style={styles.textBold}>wikipedia.org/wiki/QR_code</Text> on your computer and scan the QR code.
+                //   </Text>
+                //             }
+                // bottomContent={
+                //     <TouchableOpacity style={styles.buttonTouchable}>
+                //         <Text style={styles.buttonText}>OK. Got it!</Text>
+                //     </TouchableOpacity>
+                // }
                 />
             </SafeAreaView>
         );
